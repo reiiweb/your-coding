2735a8f590f5b740816f7d3310b028f374c2042d
rei-portfolio <87227727+rei-portfolio@users.noreply.github.com>
2021-11-11 20:35:28 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,4 +1,15 @@
 $(function () {
+  const setFillHeight = () => {
+    const vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty("--vh", `${vh}px`);
+  };
+
+  // 画面のサイズ変動があった時に高さを再計算する
+  window.addEventListener("resize", setFillHeight);
+
+  // 初期化
+  setFillHeight();
+
   // Aos
   AOS.init({
     offset: 200,
