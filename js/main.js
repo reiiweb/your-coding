2a9c1da7cc28b474f0e3c866db7e0feecd0b02f5
rei-portfolio <87227727+rei-portfolio@users.noreply.github.com>
2021-11-11 18:58:44 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,4 +1,16 @@
 $(function () {
+// 1.関数の定義
+function setHeight() {
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+}
+
+// 2.初期化
+setHeight();
+
+// 3.ブラウザのサイズが変更された時・画面の向きを変えた時に再計算する
+window.addEventListener('resize', setHeight);  
+
   // Aos
   AOS.init({
     offset: 200,
